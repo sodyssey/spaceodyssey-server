8509e1bdebe24207fe13955d08526f598616d24a
JaTin <jigyashusaini7@gmail.com>
2023-11-22 12:15:34 +0530

    modefying link that will be sent to user on requesting reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -149,7 +149,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
 
     //send it to user's email
-    const resetUrl = `${req.protocol}://${req.get('host')}/users/resetPassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/resetPassword/${resetToken}`;
     const message = `Forgot password? Sumbit a patch request with your new password and passwordConfirm to:\n
      ${resetUrl}\nPlease ignore this message if you didn't forgot the password!.`;
 
